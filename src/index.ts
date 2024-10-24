6685041ab1ff9681e2771a9036cca68de10412ac
Jon Scheiding <jon@examspark.com>
2024-10-24 13:02:06 -0400

    playing with async local storage api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("HELLO");
+import { AsyncLocalStorage } from "async_hooks";
+
+const storage = new AsyncLocalStorage<number>();
+
+const thing = async () => {
+  for (let i = 0; i < 10; i++) {
+    console.log(`Thread ${storage.getStore()}: iteration ${i}`);
+    await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+  }
+};
+
+storage.run(1, thing);
+storage.run(2, thing);
